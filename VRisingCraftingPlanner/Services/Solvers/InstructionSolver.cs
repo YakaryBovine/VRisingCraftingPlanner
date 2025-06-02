c273b7cbce28d56d5998392d4a4f7483fde3e7ae
Zakary Boven <zakaryboven@gmail.com>
2025-06-02 21:10:59 +1200

    Squished redundant craft instructions

--- VRisingCraftingPlanner/Services/Solvers/InstructionSolver.cs
+++ VRisingCraftingPlanner/Services/Solvers/InstructionSolver.cs
@@ -4,7 +4,11 @@ using VRisingCraftingPlanner.Services.Stores;
 
 namespace VRisingCraftingPlanner.Services.Solvers;
 
-public sealed class InstructionSolver(InventoryStore inventoryStore, RecipeStore recipeStore, ItemBalanceStore itemBalanceStore)
+public sealed class InstructionSolver(
+  InventoryStore inventoryStore,
+  RecipeStore recipeStore,
+  ItemBalanceStore itemBalanceStore,
+  InstructionStore instructionStore)
 {
   /// <summary>
   /// Receives the items the player would like to acquire, and the items they already have, and provides a series of
@@ -13,12 +17,10 @@ public sealed class InstructionSolver(InventoryStore inventoryStore, RecipeStore
   /// <returns>A series of tasks the player should undertake to acquire the items.</returns>
   public void Solve()
   {
-    var instructions = new List<IInstruction>();
-
     ProcessInventory();
-    ProcessCrafts(instructions);
-    ProcessMaterials(instructions);
-    PrintInstructions(instructions);
+    ProcessCrafts();
+    ProcessMaterials();
+    PrintInstructions();
   }
 
   private void ProcessInventory()
@@ -26,15 +28,15 @@ public sealed class InstructionSolver(InventoryStore inventoryStore, RecipeStore
     itemBalanceStore.Add(inventoryStore.GetInventory());
   }
 
-  private void ProcessCrafts(List<IInstruction> instructions)
+  private void ProcessCrafts()
   {
     foreach (var item in itemBalanceStore.GetAllItems().Where(x => x.ItemType.Origin == ItemOrigin.Product).ToList())
     {
-      ProcessCraft(item, instructions, 1);
+      ProcessCraft(item, 1);
     }
   }
 
-  private void ProcessCraft(Item item, List<IInstruction> instructions, int priority)
+  private void ProcessCraft(Item item, int priority)
   {
     if (item.Count < 0)
     {
@@ -46,26 +48,26 @@ public sealed class InstructionSolver(InventoryStore inventoryStore, RecipeStore
         itemBalanceStore.Add(recipe.Products);
         itemBalanceStore.Subtract(recipe.Ingredients);
       }
-      instructions.Add(new CraftInstruction(recipe, craftsNeeded, priority));
+      instructionStore.Add(new CraftInstruction(recipe, craftsNeeded, priority));
 
       foreach (var subProduct in recipe.Ingredients.Where(x => x.ItemType.Origin == ItemOrigin.Product))
       {
-        ProcessCraft(subProduct with { Count = subProduct.Count * item.Count / productCount }, instructions, priority + 1);
+        ProcessCraft(subProduct with { Count = subProduct.Count * item.Count / productCount }, priority + 1);
       }
     }
   }
   
-  private void ProcessMaterials(List<IInstruction> instructions)
+  private void ProcessMaterials()
   {
     foreach (var item in itemBalanceStore.GetAllItems().Where(x => x.ItemType.Origin == ItemOrigin.Ingredient).ToList())
       if (item.Count < 0) 
-        instructions.Add(new GatheringInstruction(item.ItemType, item.Count * -1, "Farbane"));
+        instructionStore.Add(new GatheringInstruction(item.ItemType, item.Count * -1, "Farbane"));
   }
   
-  private static void PrintInstructions(List<IInstruction> instructions)
+  private void PrintInstructions()
   {
     var count = 1;
-    foreach (var instruction in instructions.OrderByDescending(x => x.Priority))
+    foreach (var instruction in instructionStore.GetAll().OrderByDescending(x => x.Priority))
     {
       Console.WriteLine($"{count}. {instruction.Message}");
       count++;
